e327b60c5fc2dbee6b1a15b6f7dbdc01c774b146
pfed-prog <fedorovn19@gmail.com>
2022-07-09 06:55:14 +0200

    moved into one folder

--- next-app/store.js
+++
@@ -1,9 +0,0 @@
-import create from "zustand";
-
-export const useStore = create((set) => ({
-  currentUser: null,
-  setUpStore: (currentUser) =>
-    set(() => ({
-      currentUser: currentUser,
-    })),
-}));
